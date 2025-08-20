c749535267bf9b5ed6aa375df4b2901b82cdbced
Mogrul <mogrul.py@gmail.com>
2025-08-20 23:43:19 +0100

    1.21.6 -> 1.21.7

---
+++ src/main/java/net.mogrul.economy/data/MemoryData.java
@@ -0,0 +1,10 @@
+package net.mogrul.economy.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryData {
+    public static Map<String, PlayerData> players = new HashMap<>();
+    public static Map<String, MobRewardData> mobRewards = new HashMap<>();
+    public static Map<String, ShopData> shops = new HashMap<>();
+}
